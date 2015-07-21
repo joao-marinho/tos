98fb2d0b67fd5b0a1e9077847c516ecb5a5257f0
JoaoB <joao.marinho@outlook.com>
2015-07-21 14:06:08 -0300

    Added gerente namespace

---
+++ controllers/gerente/sessions.js
@@ -0,0 +1,29 @@
+module.exports = function(models, services) {
+  var Authentication = services.Authentication_for("Gerente");
+
+  return {
+    new: function(scope) {
+
+    },
+    create: function(req, res, next) {
+      var email = req.body.gerente.email;
+      var password = req.body.gerente.password;
+      var session = req.session;
+
+      return Authentication.login(email, password, session).then(function(gerente) {
+        res.redirect("/gerente");
+      }, function() {
+        // Authentication fail
+        res.redirect("/gerente/sessions/new");
+      });
+    },
+    delete: function(req, res, next) {
+      var session = req.session;
+
+      return Authentication.logout(session).then(function() {
+        res.redirect("/gerente/sessions/new");
+      });
+    }
+  };
+
+};
