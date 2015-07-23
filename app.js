13a6c7697dafe2ead592fa5d64e10a96a3dd9bb7
JoaoB <joao.marinho@outlook.com>
2015-07-23 15:08:44 -0300

    Installed connect-assets; Configured assests folder

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+app.use(require("connect-assets")());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
