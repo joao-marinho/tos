e025dc382533e843bdde7f59f75b379a30872482
JoaoB <joao.marinho@outlook.com>
2015-07-30 13:38:27 -0300

    Created Equipe.findByTecnicosOrCreate

--- models/equipe.js
+++ models/equipe.js
@@ -4,14 +4,19 @@ module.exports = function(daos, BasicModel) {
   var equipeDao = daos.Equipe;
   var Equipe = new BasicModel(equipeDao);
 
-  Equipe.findByTecnicosOrCreate = function(tecnico1, tecnico2) {
+  Equipe.findByTecnicosOrCreate = function(tecnico1, tecnico2, gerenteId) {
     var self = this;
 
-    self._dao.where([
+    return self._dao.where([
       {primeiro_tecnico_id: tecnico1.id, segundo_tecnico_id: tecnico2.id},
       {primeiro_tecnico_id: tecnico2.id, segundo_tecnico_id: tecnico1.id},
     ]).then(function(result) {
-      console.log(result);
+      if(result.rows.length == 0) {
+        return self.create({primeiro_tecnico_id: tecnico1.id, segundo_tecnico_id: tecnico2.id, gerente_id: gerenteId});
+      }
+      else {
+        return new Equipe(result.rows[0]);
+      }
     });
   };
 
