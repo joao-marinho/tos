c68f23d91e7d3d26a54a7c628c7ed6e0b58d8073
JoaoB <joao.marinho@outlook.com>
2015-08-02 16:02:19 -0300

    Functions to change status of ordem de servico; tecnico.ordensDeServico

--- models/tecnico.js
+++ models/tecnico.js
@@ -1,8 +1,24 @@
 var q = require("q");
 
-module.exports = function(daos, BasicModel) {
+module.exports = function(daos, BasicModel, models) {
   var tecnicoDao = daos.Tecnico;
   var Tecnico = new BasicModel(tecnicoDao);
+
+  Tecnico.prototype.ordensDeServico = function() {
+    var self = this;
+    var OrdemDeServico = models().OrdemDeServico;
+    var Equipe = models().Equipe;
+
+    return Equipe.where([{primeiro_tecnico_id: self.id}, {segundo_tecnico_id: self.id}]).then(function(equipes) {
+      if(equipes.length == 0) {
+        return [];
+      }
+      var osQuery = equipes.map(function(equipe) {
+        return {equipe_id: equipe.id};
+      });
+      return OrdemDeServico.where(osQuery);
+    });
+  };
 
   return Tecnico;
 };
